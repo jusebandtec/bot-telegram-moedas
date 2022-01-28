4ebff545563aafcbf0fbe282fd37773c10791ac7
José Paulo <jose.silva@bandtec.com.br>
2022-01-28 15:46:27 -0300

    Criando projeto

---
+++ Bot-Telegram-Para-Cotar-principais-moedas/src/main/java/br/com/investimentos/Bot/Telegram/Para/Cotar/principais/moedas/BotTelegramParaCotarPrincipaisMoedasApplication.java
@@ -0,0 +1,13 @@
+package br.com.investimentos.Bot.Telegram.Para.Cotar.principais.moedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotTelegramParaCotarPrincipaisMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotTelegramParaCotarPrincipaisMoedasApplication.class, args);
+	}
+
+}
---
+++ Bot-Telegram-Para-Cotar-principais-moedas/src/test/java/br/com/investimentos/Bot/Telegram/Para/Cotar/principais/moedas/BotTelegramParaCotarPrincipaisMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.investimentos.Bot.Telegram.Para.Cotar.principais.moedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotTelegramParaCotarPrincipaisMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
