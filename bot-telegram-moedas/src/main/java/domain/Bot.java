3746051c3aed702f03a770453fd1917d7576570a
José Paulo <jose.silva@bandtec.com.br>
2022-01-29 00:30:48 -0300

    Removendo projeto de console

--- bot-telegram-moedas/src/main/java/domain/Bot.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public class Bot {
-
-    public static final String BOT_TOKEN = "5282374021:AAEAkS1TLOR-U6M5wCB4NEIDfknwVEmiDKo";
-    public static final String BOT_USERNAME = "ValorMoedasBot";
-}
