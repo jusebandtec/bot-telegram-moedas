672d5b931e7a5a6b02557b172cc903aaa008efb3
José Paulo <jose.silva@bandtec.com.br>
2022-01-29 01:20:49 -0300

    Trazendo projeto para raiz

--- BOT-Telegram/src/main/java/br/com/obrigadobombcrypto/bot/domain/Bot.java
+++
@@ -1,7 +0,0 @@
-package br.com.obrigadobombcrypto.bot.domain;
-
-public class Bot {
-
-    public static final String BOT_TOKEN = "5282374021:AAEAkS1TLOR-U6M5wCB4NEIDfknwVEmiDKo";
-    public static final String BOT_USERNAME = "ValorMoedasBot";
-}
